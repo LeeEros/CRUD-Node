05d6616e7e736a437025dc3cf57511defa6f021e
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2024-05-29 11:26:24 -0300

    Fix: remove MariaDB, docker

--- server.js
+++ server.js
@@ -1,30 +1,8 @@
-const express = require('express');
-const mysql = require('mysql2');
-const app = express();
-const port = 3000;
+import app from "./app";
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'crud'
-});
-
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to MariaDB:', err.stack);
-        return;
-    }
-    console.log('Connected to MariaDB as id', connection.threadId);
-});
-
-app.get('/', (req, res) => {
-    connection.query('SELECT NOW()', (err, results) => {
-        if (err) throw err;
-        res.send(results);
-    });
-});
+const port = 3001;
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Escutando na porta ${port}`);
+    console.log(`http://localhost:${port}`);
 });
